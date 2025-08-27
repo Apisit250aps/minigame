ab7ce6aabf8b185e9bd26121cc41349b85d559ad
YUapisit <aps.apisit250@gmail.com>
2025-08-27 10:32:20 +0700

    feat: add Topic and Question models with migration

--- quest/models.py
+++ quest/models.py
@@ -1,3 +1,38 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)  # insert ครั้งแรก
+    updated_at = models.DateTimeField(auto_now=True)  # อัปเดตทุกครั้ง
+
+    def __str__(self):
+        return self.name
+
+
+class Question(models.Model):
+    class Difficulty(models.IntegerChoices):
+        EASY = (1,)
+        MEDIUM = (2,)
+        HARD = (3,)
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    question = models.TextField()  # คำถาม
+    answer = models.TextField()  # เฉลย
+    explanation = models.TextField(null=True, blank=True)  # อธิบายคำตอบ
+    difficulty = models.PositiveSmallIntegerField(
+        choices=Difficulty.choices, default=Difficulty.EASY
+    )
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+
+    # เทียบกับ CURRENT_TIMESTAMP และ ON UPDATE CURRENT_TIMESTAMP
+    created_at = models.DateTimeField(auto_now_add=True)  # insert ครั้งแรก
+    updated_at = models.DateTimeField(auto_now=True)  # อัปเดตทุกครั้ง
+
+    def __str__(self):
+        return (self.question[:60] + "…") if len(self.question) > 60 else self.question
